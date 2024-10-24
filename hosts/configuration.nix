bc6903f6517c3668bd90149adf9c8b422c2e4206
Thony Price <thony@thonys-mbp.lan>
2024-10-24 22:14:59 +0200

    Add longhorn fixes

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -50,6 +50,10 @@
     clusterInit = (meta.hostname == "homelab-0");
   };
 
+  # Fixes for longhorn
+  systemd.tmpfiles.rules =
+    [ "L+ /usr/local/bin - - - - /run/current-system/sw/bin/" ];
+  virtualisation.docker.logDriver = "json-file";
   services.openiscsi = {
     enable = true;
     name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
